687143008032ce068dba46a11d85f69a30214d12
ViniciusCarvalhoLima <128156331+ViniciusCarvalhoLima@users.noreply.github.com>
2023-04-02 13:42:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ if idade < 0:
     print("impossível!")
     print("não precisa se alistar.")
 else:
+    if idade < 18:
+        print("não precisa se alistar.")
     if idade > 18 and (idade < 65):
         print("Não esqueça de votar na próxima eleição.")
     if idade > 65:
